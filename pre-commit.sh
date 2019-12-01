0557646b5e13da2043a3abef26e5c4b7fe37bcce
FeuRenard <feurenard@posteo.de>
2019-12-01 13:36:04 +0100

    :wrench: Add clippy to git hook pre-commit

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+
+cargo clippy
+
+if [ ! $? -eq 0 ]; then
+    echo "Fix linter errors before committing"
+    exit 1
+fi
+
 cargo fmt --all -- --check --quiet
-if [ $? -eq 0 ]; then
-    exit 0
-else
+
+if [ ! $? -eq 0 ]; then
     echo "Run 'cargo fmt --all' before committing"
     exit 1
 fi
+
+exit 0
 
