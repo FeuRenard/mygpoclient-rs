5511a69f1ce0c5a472abe0ca224987160294efa3
FeuRenard <feurenard@posteo.de>
2019-12-01 13:02:39 +0100

    :wrench: Activate many rustc lints

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
+#![deny(
+    warnings,
+    future_incompatible,
+    nonstandard_style,
+    rust_2018_compatibility,
+    rust_2018_idioms,
+    unused,
+    macro_use_extern_crate,
+    non_ascii_idents,
+    single_use_lifetimes,
+    trivial_casts,
+    trivial_numeric_casts,
+    unreachable_pub,
+    unsafe_code,
+    unused_import_braces,
+    unused_lifetimes,
+    unused_qualifications
+)]
+
 pub use error::Error;
 
 mod error;
