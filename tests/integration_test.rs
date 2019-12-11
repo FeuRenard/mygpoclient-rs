7404d0f4b7519a165064514c1684876cb83d234c
FeuRenard <feurenard@posteo.de>
2019-12-11 20:52:14 +0100

    :construction_worker: Add Travis config

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,36 +1,51 @@
 extern crate libmygpo_rs;
 
+use std::env;
+
 use libmygpo_rs::subscription;
 use libmygpo_rs::Error;
 
-const USERNAME: &'static str = "<insert username>";
-const PASSWORD: &'static str = "<insert password>";
 const DEVICEID: &'static str = "randomdeviceid"; // TODO
 const DUMMY_PODCAST_URL: &'static str = "http://ubuntupodcast.org/feed/";
 
 #[test]
 fn test_subscription() -> Result<(), Error> {
-    let subscriptions = subscription::get(USERNAME, PASSWORD, DEVICEID)?;
+    let username = env::var("GPODDER_NET_USERNAME").unwrap();
+    let password = env::var("GPODDER_NET_PASSWORD").unwrap();
+
+    let subscriptions = subscription::get(username.as_str(), password.as_str(), DEVICEID)?;
 
     if subscriptions.contains(&get_dummy_url()) {
-        add_and_assert_contains(remove_and_assert_gone(subscriptions)?)?;
+        add_and_assert_contains(
+            remove_and_assert_gone(subscriptions, username.as_str(), password.as_str())?,
+            username.as_str(),
+            password.as_str(),
+        )?;
     } else {
-        remove_and_assert_gone(add_and_assert_contains(subscriptions)?)?;
+        remove_and_assert_gone(
+            add_and_assert_contains(subscriptions, username.as_str(), password.as_str())?,
+            username.as_str(),
+            password.as_str(),
+        )?;
     }
 
     Ok(())
 }
 
-fn add_and_assert_contains(mut subscriptions: Vec<String>) -> Result<Vec<String>, Error> {
+fn add_and_assert_contains(
+    mut subscriptions: Vec<String>,
+    username: &str,
+    password: &str,
+) -> Result<Vec<String>, Error> {
     subscriptions.push(get_dummy_url());
-    subscription::put(&subscriptions, USERNAME, PASSWORD, DEVICEID)?;
+    subscription::put(&subscriptions, username, password, DEVICEID)?;
 
-    let subscriptions_after_addition = subscription::get(USERNAME, PASSWORD, DEVICEID)?;
+    let subscriptions_after_addition = subscription::get(username, password, DEVICEID)?;
     assert!(subscriptions_after_addition.contains(&get_dummy_url()));
 
     assert_eq!(
         1,
-        subscription::Subscription::get_all(USERNAME, PASSWORD)?
+        subscription::Subscription::get_all(username, password)?
             .iter()
             .filter(|s| s.url == get_dummy_url())
             .count()
@@ -39,7 +54,11 @@ fn add_and_assert_contains(mut subscriptions: Vec<String>) -> Result<Vec<String>
     Ok(subscriptions_after_addition)
 }
 
-fn remove_and_assert_gone(subscriptions: Vec<String>) -> Result<Vec<String>, Error> {
+fn remove_and_assert_gone(
+    subscriptions: Vec<String>,
+    username: &str,
+    password: &str,
+) -> Result<Vec<String>, Error> {
     subscription::put(
         subscriptions
             .iter()
@@ -47,12 +66,12 @@ fn remove_and_assert_gone(subscriptions: Vec<String>) -> Result<Vec<String>, Err
             .cloned()
             .collect::<Vec<String>>()
             .as_ref(),
-        USERNAME,
-        PASSWORD,
+        username,
+        password,
         DEVICEID,
     )?;
 
-    let subscriptions_after_removal = subscription::get(USERNAME, PASSWORD, DEVICEID)?;
+    let subscriptions_after_removal = subscription::get(username, password, DEVICEID)?;
     assert!(!subscriptions_after_removal.contains(&get_dummy_url()));
     Ok(subscriptions_after_removal)
 }
