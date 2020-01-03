56cf0c3c7a48a514fe60d015d86ab52c306a4644
FeuRenard <feurenard@posteo.de>
2020-01-03 18:16:13 +0100

    :sparkles: Implement Update Device Data API

--- src/device.rs
+++ src/device.rs
@@ -1,9 +1,10 @@
 use crate::client::{AuthenticatedClient, DeviceClient};
 use crate::Error;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
+// TODO #[serde(rename_all = "lowercase")]
 #[allow(non_camel_case_types)]
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 pub enum DeviceType {
     desktop,
     laptop,
@@ -16,14 +17,59 @@ pub enum DeviceType {
 pub struct Device {
     pub id: String,
     pub caption: String,
+    // TODO #[serde(rename(serialize = "type", deserialize = "type"))]
     pub r#type: DeviceType,
     pub subscriptions: u16,
 }
 
+#[derive(Serialize)]
+pub(crate) struct DeviceData {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub(crate) caption: Option<String>,
+    #[serde(rename(serialize = "type", deserialize = "type"))]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub(crate) device_type: Option<DeviceType>,
+}
+
 /// [Device API](https://gpoddernet.readthedocs.io/en/latest/api/reference/devices.html)
 pub trait Devices: ListDevices {}
-// TODO https://gpoddernet.readthedocs.io/en/latest/api/reference/devices.html#update-device-data
 // TODO https://gpoddernet.readthedocs.io/en/latest/api/reference/devices.html#get-device-updates
+
+pub trait UpdateDeviceData {
+    /// Update Device Data
+    ///
+    /// # Returns
+    ///
+    /// A `Result` which is
+    ///
+    /// - `Ok`
+    /// - `Err`: A wrapped JSON or network error
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use libmygpo_rs::DeviceClient;
+    /// use libmygpo_rs::device::{DeviceType,UpdateDeviceData};
+    ///
+    /// # let username = std::env::var("GPODDER_NET_USERNAME").unwrap();
+    /// # let password = std::env::var("GPODDER_NET_PASSWORD").unwrap();
+    /// # let deviceid = std::env::var("GPODDER_NET_DEVICEID").unwrap();
+    /// #
+    /// let client = DeviceClient::new(&username, &password, &deviceid);
+    ///
+    /// client.update_device_data("My Phone".to_owned(), None)?;
+    /// # Ok::<(), libmygpo_rs::Error>(())
+    /// ```
+    ///
+    /// # See also
+    ///
+    /// - [gpodder.net API Documentation](https://gpoddernet.readthedocs.io/en/latest/api/reference/devices.html#update-device-data)
+    fn update_device_data<T: Into<Option<String>>, U: Into<Option<DeviceType>>>(
+        &self,
+        caption: T,
+        device_type: U,
+    ) -> Result<(), Error>;
+}
 
 pub trait ListDevices {
     /// List Devices
@@ -55,6 +101,27 @@ pub trait ListDevices {
     ///
     /// - [gpodder.net API Documentation](https://gpoddernet.readthedocs.io/en/latest/api/reference/devices.html#list-devices)
     fn list_devices(&self) -> Result<Vec<Device>, Error>;
+}
+
+impl UpdateDeviceData for DeviceClient {
+    fn update_device_data<T: Into<Option<String>>, U: Into<Option<DeviceType>>>(
+        &self,
+        caption: T,
+        device_type: U,
+    ) -> Result<(), Error> {
+        let input = DeviceData {
+            caption: caption.into(),
+            device_type: device_type.into(),
+        };
+        self.post(
+            &format!(
+                "https://gpodder.net/api/2/devices/{}/{}.json",
+                self.authenticated_client.username, self.device_id
+            ),
+            &input,
+        )?;
+        Ok(())
+    }
 }
 
 impl ListDevices for AuthenticatedClient {
