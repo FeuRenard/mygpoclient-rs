87ee9892327a8ec48f4f35aa3f9f0c21e3a1fa08
FeuRenard <feurenard@posteo.de>
2020-05-02 15:02:34 +0200

    :white_check_mark: Test DeviceClient ListDevices implementation

---
+++ tests/device.rs
@@ -0,0 +1,19 @@
+extern crate mygpoclient;
+
+use std::env;
+
+use mygpoclient::client::DeviceClient;
+use mygpoclient::device::ListDevices;
+use mygpoclient::error::Error;
+
+#[test]
+fn test_retrieve_top_tags_device_client() -> Result<(), Error> {
+    let username = env::var("GPODDER_NET_USERNAME").unwrap();
+    let password = env::var("GPODDER_NET_PASSWORD").unwrap();
+    let deviceid = env::var("GPODDER_NET_DEVICEID").unwrap();
+
+    let client = DeviceClient::new(&username, &password, &deviceid);
+    client.list_devices()?;
+
+    Ok(())
+}
