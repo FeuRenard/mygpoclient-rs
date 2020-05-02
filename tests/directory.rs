764cf69bd8e62fbf8d67499b585a8ec0904008f0
FeuRenard <feurenard@posteo.de>
2020-05-02 12:27:47 +0200

    :white_check_mark: Test DeviceClient implementations

---
+++ tests/directory.rs
@@ -0,0 +1,47 @@
+extern crate mygpoclient;
+
+use std::env;
+use url::Url;
+
+use mygpoclient::client::DeviceClient;
+use mygpoclient::directory::RetrievePodcastData;
+use mygpoclient::directory::RetrievePodcastsForTag;
+use mygpoclient::directory::RetrieveTopTags;
+use mygpoclient::error::Error;
+
+#[test]
+fn test_retrieve_top_tags_device_client() -> Result<(), Error> {
+    let client = get_device_client();
+    let max_results = 5;
+    let tags = client.retrieve_top_tags(max_results)?;
+    assert_eq!(max_results as usize, tags.len());
+
+    Ok(())
+}
+
+#[test]
+fn test_retrieve_podcasts_for_tag_device_client() -> Result<(), Error> {
+    let client = get_device_client();
+    let max_results = 5;
+    let podcasts = client.retrieve_podcasts_for_tag("new", max_results)?;
+    assert!(podcasts.len() <= max_results as usize);
+
+    Ok(())
+}
+
+#[test]
+fn test_retrieve_podcast_data_device_client() -> Result<(), Error> {
+    let client = get_device_client();
+    let url = Url::parse("http://feeds.feedburner.com/coverville").unwrap();
+    client.retrieve_podcast_data(url)?;
+
+    Ok(())
+}
+
+fn get_device_client() -> DeviceClient {
+    let username = env::var("GPODDER_NET_USERNAME").unwrap();
+    let password = env::var("GPODDER_NET_PASSWORD").unwrap();
+    let deviceid = env::var("GPODDER_NET_DEVICEID").unwrap();
+
+    DeviceClient::new(&username, &password, &deviceid)
+}
